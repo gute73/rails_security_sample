a6e9751a26ba20f089b7d5ebfb05ec91e600f361
Mark D. King <kingmarkd@gmail.com>
2019-03-08 11:00:45 -0500

    Typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-      loging(@user)
+      login(@user)
   		flash[:success] = "Account successfully created."
   		redirect_to @user
   	else
