a6f8c4acfdd00f453d4347bbfa53e54531db7b50
Mark D. King <kingmarkd@gmail.com>
2019-03-08 10:53:33 -0500

    Fixed signup error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      loging(@user)
   		flash[:success] = "Account successfully created."
   		redirect_to @user
   	else
